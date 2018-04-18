d46e68ca8e769d47892d99c8042c9adfcd54cbf5
Martin Uribe <clamytoe@users.noreply.github.com>
2018-04-18 14:12:31 -0500

    Turned into an object

--- passwd_validate.py
+++ passwd_validate.py
@@ -4,100 +4,106 @@ from string import ascii_uppercase as uppercase
 from string import digits
 from string import punctuation as special
 
+
 # special = '!.*;$#@'  # These were specifically mentioned
 
-ACTUAL_NAME = ''
-USER_NAME = ''
-
-CHAR_TYPES = [digits, lowercase, special, uppercase]
-DICTIONARY = 'util/dictionary.txt'
-PHPBB = 'util/phpbb.txt'
-ROCKYOU = 'util/rockyou.txt'
-DICTS = [DICTIONARY, PHPBB, ROCKYOU]
+class User:
+    def __init__(self, fullname, username):
+        self.first_name = fullname.split()[0]
+        self.last_name = fullname.split()[-1]
+        self.username = username
 
 
-def read_file(filename):
-    try:
-        with open(filename) as file:
-            for line in file:
-                yield line.rstrip()
-    except UnicodeDecodeError:
-        # LOL, like my hack around this one??
-        yield 'error'
+class ValidatePassword(User):
+    CHAR_TYPES = [digits, lowercase, special, uppercase]
+    DICTIONARY = 'util/dictionary.txt'
+    PHPBB = 'util/phpbb.txt'
+    ROCKYOU = 'util/rockyou.txt'
+    DICTS = [DICTIONARY, PHPBB, ROCKYOU]
 
+    def __init__(self, fullname, username, password):
+        User.__init__(self, fullname, username)
+        self.password = password
 
-def not_in_dict(password):
-    for passwd_file in DICTS:
-        dict_words = read_file(passwd_file)
-        for word in dict_words:
-            if 'dictionary' in passwd_file and len(word) < 5:
-                break
-            if password in word:
-                return False
-    return True
+    @staticmethod
+    def _read_file(filename):
+        try:
+            with open(filename) as file:
+                for line in file:
+                    yield line.rstrip()
+        except UnicodeDecodeError:
+            # LOL, like my hack around this one??
+            yield 'error'
 
-
-def check_length(password):
-    return False if len(password) < 10 else True
-
-
-def check_characters(password):
-    found = [0] * len(CHAR_TYPES)
-    count = 0
-
-    for i, char_type in enumerate(CHAR_TYPES):
-        for char in password:
-            if found[i] == 0:
-                if char in char_type:
-                    count += 1
-                    found[i] = 1
+    def _not_in_dict(self):
+        for passwd_file in self.DICTS:
+            dict_words = self._read_file(passwd_file)
+            for word in dict_words:
+                if 'dictionary' in passwd_file and len(word) < 5:
                     break
-    return True if count >= 3 else False
+                if self.password in word:
+                    return False
+        return True
 
+    def _check_length(self):
+        return False if len(self.password) < 10 else True
 
-def not_using_username(password):
-    global USER_NAME
-    if not USER_NAME:
-        USER_NAME = input('username: ')
+    def _check_characters(self):
+        found = [0] * len(self.CHAR_TYPES)
+        count = 0
 
-    return False if USER_NAME.lower() in password.lower() else True
+        for i, char_type in enumerate(self.CHAR_TYPES):
+            for char in self.password:
+                if found[i] == 0:
+                    if char in char_type:
+                        count += 1
+                        found[i] = 1
+                        break
+        return True if count >= 3 else False
 
-
-def not_using_name(password):
-    global ACTUAL_NAME
-    if not ACTUAL_NAME:
-        ACTUAL_NAME = input('First and last name: ')
-
-    first_name = ACTUAL_NAME.split()[0]
-    last_name = ACTUAL_NAME.split()[-1]
-
-    for name in [first_name, last_name]:
-        if name.lower() in password.lower():
+    def _not_using_username(self):
+        if self.username.lower() in self.password.lower():
             return False
-    return True
+        else:
+            return True
 
+    def _not_using_name(self):
+        for name in [self.first_name, self.last_name]:
+            if name.lower() in self.password.lower():
+                return False
+        return True
 
-def validate(password):
-    checks = [
-        not_in_dict,
-        check_length,
-        check_characters,
-        not_using_username,
-        not_using_name,
-    ]
+    @property
+    def validate(self):
+        checks = [
+            self._not_in_dict,
+            self._check_length,
+            self._check_characters,
+            self._not_using_username,
+            self._not_using_name,
+        ]
 
-    for check in checks:
-        valid = check(password)
-        if not valid:
-            return False
-    return True
+        for check in checks:
+            valid = check()
+            if not valid:
+                return False
+        return True
 
 
 if __name__ == '__main__':
-    while True:
-        PASSWD = getpass('password: ')
-        if validate(PASSWD):
-            print('Your password is good!')
-            break
-        else:
-            print(f'Your password is invalid! {PASSWD}')
+    try:
+        name = input('First and Last name: ')
+        account = input('Username: ')
+
+        while True:
+            password = getpass('Password: ')
+            passwd = ValidatePassword(name, account, password)
+
+            if passwd.validate:
+                print(f'{password} is good!')
+                break
+            else:
+                print(f'{password} is invalid!')
+    except KeyboardInterrupt:
+        print('\n\nProgram aborted by user. Exiting...\n')
+        exit()
