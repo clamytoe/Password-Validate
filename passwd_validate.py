0da015fbe84e7a058675f287c4f2968029a16640
Martin Uribe <clamytoe@users.noreply.github.com>
2018-04-18 13:02:50 -0500

    Initial script upload

---
+++ passwd_validate.py
@@ -0,0 +1,103 @@
+from getpass import getpass
+from string import ascii_letters as lowercase
+from string import ascii_uppercase as uppercase
+from string import digits
+from string import punctuation as special
+
+# special = '!.*;$#@'  # These were specifically mentioned
+
+actualname = ''
+username = ''
+
+CHAR_TYPES = [digits, lowercase, special, uppercase]
+DICTIONARY = 'util/dictionary.txt'
+PHPBB = 'util/phpbb.txt'
+rockyou = 'util/rockyou.txt'
+DICTS = [DICTIONARY, PHPBB, rockyou]
+
+
+def read_file(file):
+    try:
+        with open(file) as f:
+            for line in f:
+                yield line.rstrip()
+    except UnicodeDecodeError:
+        # LOL, like my hack around this one??
+        yield 'error'
+
+
+def not_in_dict(password):
+    for passwd_file in DICTS:
+        dict_words = read_file(passwd_file)
+        for word in dict_words:
+            if 'dictionary' in passwd_file and len(word) < 5:
+                break
+            if password in word:
+                return False
+    return True
+
+
+def check_length(password):
+    return False if len(password) < 10 else True
+
+
+def check_characters(password):
+    found = [0] * len(CHAR_TYPES)
+    count = 0
+
+    for i, char_type in enumerate(CHAR_TYPES):
+        for char in password:
+            if found[i] == 0:
+                if char in char_type:
+                    count += 1
+                    found[i] = 1
+                    break
+    return True if count >=3 else False
+
+
+def not_using_username(password):
+    global username
+    if not username:
+        username = input('username: ')
+
+    return False if username.lower() in password.lower() else True
+
+
+def not_using_name(password):
+    global actualname
+    if not actualname:
+        actualname = input('First and last name: ')
+
+    firstname = actualname.split()[0]
+    lastname = actualname.split()[-1]
+
+    for name in [firstname, lastname]:
+        if name.lower() in password.lower():
+            return False
+    return True
+
+
+def validate(password):
+    checks = [
+        not_in_dict,
+        check_length,
+        check_characters,
+        not_using_username,
+        not_using_name,
+    ]
+
+    for check in checks:
+        valid = check(password)
+        if not valid:
+            return False
+    return True
+
+
+if __name__ == '__main__':
+    while True:
+        passwd = getpass('password: ')
+        if validate(passwd):
+            print(f'Your password is good!: {passwd}')
+            break
+        else:
+            print(f'Your password sucks! {passwd}')
