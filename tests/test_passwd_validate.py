d8abbf940bcbf58518de03531578380fe02ba6ef
Martin Uribe <mohhinder@gmail.com>
2018-05-16 12:15:24 -0500

    Started adding tests

--- tests/test_passwd_validate.py
+++ tests/test_passwd_validate.py
@@ -2,12 +2,32 @@
 """
 test_passwd_validate
 """
+import pytest
 from pathlib import Path
-from passwd_validate.app import Account, load_data
 
-TEST_FILE = ".test_file"
-test_file = Path(TEST_FILE)
+from passwd_validate.account import Account, PASSWD_FILE
+from passwd_validate.app import (
+    check_password, get_name, get_username, load_data)
+from passwd_validate.utils import hashit, not_in_dict, read_file
+
+TEST_FILE = Path(".test_file")
+NAME = "John Doe"
+USERNAME = "e7654321"
 
 
-def test_account_creation():
-    pass
+@pytest.fixture
+def dummy_account():
+    return Account(NAME, USERNAME)
+
+
+def test_failed_account_creation():
+    with pytest.raises(TypeError):
+        Account()
+
+
+def test_account_creation(dummy_account):
+    assert isinstance(dummy_account, Account)
+    assert dummy_account.first_name == "John"
+    assert dummy_account.last_name == "Doe"
+    assert dummy_account.username == "e7654321"
+    assert len(dummy_account.used_passwords) == 0
