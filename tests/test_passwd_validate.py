cca319fac5e4ee69f41050587bc82e404d3526af
Martin Uribe <mohhinder@gmail.com>
2018-05-16 09:44:25 -0500

    Started tests framework

---
+++ tests/test_passwd_validate.py
@@ -0,0 +1,13 @@
+# _*_ coding: utf-8 _*_
+"""
+test_passwd_validate
+"""
+from pathlib import Path
+from passwd_validate.app import Account, load_data
+
+TEST_FILE = ".test_file"
+test_file = Path(TEST_FILE)
+
+
+def test_account_creation():
+    pass
