dd0c5f82669d58b49199a20be58d13b77b10e68c
Martin Uribe <mohhinder@gmail.com>
2018-05-16 06:54:36 -0500

    Changed _check_length into a static method

--- passwd_validate.py
+++ passwd_validate.py
@@ -74,7 +74,8 @@ class Account:
             return age >= 365
         return True
     
-    def _check_length(self, password):
+    @staticmethod
+    def _check_length(password):
         """
         Checks to see if the password meets the minimum length policy.
         :param password: String of the password
